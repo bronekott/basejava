14ff3ccae7497175e9b4fdb2b6e1f404d54ebc82
bronekott <bronekott@ya.ru>
2022-03-26 13:57:39 +0300

    Task 1 - first version. ArrayStorage main methods created.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,55 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size;
 
     void clear() {
+        //storage = new Resume[10000];
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        storage[size] = resume;
+        size++;
     }
 
     Resume get(String uuid) {
+        //Resume r = new Resume();
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString() == uuid) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString() == uuid) {
+                for (int j = i; j < size - 1; j++) {
+                    storage[j] = storage[j + 1];
+                }
+                size--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            resumes[i] = storage[i];
+        }
+        //resumes = Arrays.stream(storage).filter(s -> Objects.nonNull(s)).toArray(Resume[]::new);
+        return resumes;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
