b8ad1f1dceb5715f535e6027fe30cb3997cbef83
bronekott <bronekott@ya.ru>
2022-04-02 13:57:13 +0300

    Task 3 - first version. Template method pattern implementation.

--- src/com/basejava/webapp/storage/SortedArrayStorage.java
+++ src/com/basejava/webapp/storage/SortedArrayStorage.java
@@ -16,15 +16,9 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     @Override
     protected void insertElement(int index, Resume resume) {
         int insertIndex = -index - 1;
-        //тут получается массив 1,2,null,3 и вместо null вставляем наше резюме на след. строке
         System.arraycopy(storage, insertIndex, storage, insertIndex + 1, size - insertIndex);
-        // почему если эту строку закомментировать, то будет NULLPointException? Как я понял - из-за того что null невозможно compareTo с добавляемым элементом
         storage[insertIndex] = resume;
 
-        // почему вот этот код тоже падает с NULLPointException?
-        //for(Resume r:storage) {
-        //    System.out.println(r.getUuid());
-        //}
     }
 
 }
