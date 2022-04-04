f530105ccd1175a6a649bcdef2b90bd39318c902
bronekott <bronekott@ya.ru>
2022-04-04 18:10:15 +0300

    Task 4 - Testing. Test classes created; MainReflection created.

--- src/com/basejava/webapp/MainReflection.java
+++ src/com/basejava/webapp/MainReflection.java
@@ -3,16 +3,20 @@ package com.basejava.webapp;
 import com.basejava.webapp.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         System.out.println(field.get(r));
         field.set(r, "newUUID");
-        //TODO : invoke r.toString via reflection
         System.out.println(r);
+
+        Method method = r.getClass().getDeclaredMethod("toString",null);
+        System.out.println(method.invoke(r,null));
     }
 }
