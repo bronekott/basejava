d1222a3d0c92cf9e1302ccf8005aee4e77d85278
bronekott <bronekott@ya.ru>
2022-04-28 21:20:45 +0300

    Task 5 - ArrayList and HashMap. AbstractStorageTest, MapStorageTest, ListStorageTest created.

--- test/com/basejava/webapp/storage/AbstractStorageTest.java
+++ test/com/basejava/webapp/storage/AbstractStorageTest.java
@@ -1,49 +1,75 @@
 package com.basejava.webapp.storage;
 
-import org.junit.jupiter.api.Test;
+import com.basejava.webapp.model.Resume;
+import org.junit.Before;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-class AbstractStorageTest {
+public class AbstractStorageTest {
     private Storage storage;
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
+    private static final String UUID_4 = "uuid4";
+
+    private static final Resume RESUME_1 = new Resume(UUID_1);
+    private static final Resume RESUME_2 = new Resume(UUID_2);
+    private static final Resume RESUME_3 = new Resume(UUID_3);
+    private static final Resume RESUME_4 = new Resume(UUID_4);
 
     public AbstractStorageTest(Storage storage) {
         this.storage = storage;
     }
 
-    @Test
-    void findSearchKey() {
+    @Before
+    public void setUp() throws Exception {
+        storage.clear();
+        storage.save(RESUME_1);
+        storage.save(RESUME_2);
+        storage.save(RESUME_3);
     }
 
     @Test
-    void updateResume() {
+    public void clear() {
+        storage.clear();
+        assertEquals(0, storage.size());
     }
 
     @Test
-    void saveResume() {
+    public void findSearchKey() {
     }
 
     @Test
-    void getResume() {
+    public void updateResume() {
     }
 
     @Test
-    void deleteResume() {
+    public void saveResume() {
     }
 
     @Test
-    void update() {
+    public void getResume() {
     }
 
     @Test
-    void save() {
+    public void deleteResume() {
     }
 
     @Test
-    void get() {
+    public void update() {
     }
 
     @Test
-    void delete() {
+    public void save() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void delete() {
     }
 }
+
